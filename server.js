ecf3101366676bf553a671a99d51a90fbcca0daa
Zachary Crumbo <zcrumbo@mac.com>
2017-03-09 20:18:22 -0800

    rolled back server change

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
 'use strict';
 
 const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
+const cors = require('cors');
 const debug = require('debug')('cf-gram:server');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const authRouter = require('./route/auth-router.js');
-const errors = require('./lib/error-middleware.js');
 const galleryRouter = require('./route/gallery-router.js');
 const picRouter = require('./route/pic-router.js');
-
-dotenv.load(); //loads .env file for environment variables
+const errors = require('./lib/error-middleware.js');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI);//putting this inside .env
+dotenv.load();
 
-let morganFormat = process.env.PRODUCTION ? 'common' : 'dev';
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(cors());
-app.use(morgan(morganFormat));
+app.use(morgan('dev'));
 app.use(authRouter);
 app.use(galleryRouter);
 app.use(picRouter);
 app.use(errors);
+
 
 
 const server = module.exports = app.listen(PORT, () => debug('server up:', PORT));
