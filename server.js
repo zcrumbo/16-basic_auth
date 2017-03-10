1b8bece2c701450ab3509f6ccc331a5df1284838
Zachary Crumbo <zcrumbo@mac.com>
2017-03-09 20:03:38 -0800

    updated server

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 'use strict';
 
 const express = require('express');
-const morgan = require('morgan');
 const cors = require('cors');
-const debug = require('debug')('cf-gram:server');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
+const debug = require('debug')('cf-gram:server');
 
 const authRouter = require('./route/auth-router.js');
+const errors = require('./lib/error-middleware.js');
 const galleryRouter = require('./route/gallery-router.js');
 const picRouter = require('./route/pic-router.js');
-const errors = require('./lib/error-middleware.js');
+
+dotenv.load(); //loads .env file for environment variables
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-dotenv.load();
+mongoose.connect(process.env.MONGODB_URI);//putting this inside .env
 
-mongoose.connect(process.env.MONGODB_URI);
+let morganFormat = process.env.PRODUCTION ? 'common' : 'dev';
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan(morganFormat));
 app.use(authRouter);
 app.use(galleryRouter);
 app.use(picRouter);
 app.use(errors);
-
 
 
 const server = module.exports = app.listen(PORT, () => debug('server up:', PORT));
